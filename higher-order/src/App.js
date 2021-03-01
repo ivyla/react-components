15e1196e0c1276c32d4ef55b78e2fa94ad717491
Ivy <ivyla3@gmail.com>
2021-02-28 20:22:48 -0800

    simple example with spread operator

--- higher-order/src/App.js
+++ higher-order/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+// Use hooks to init the state for the click right and click left buttons
+// Use the spread syntax to initialize and assign the props {//variables here//}
+
+const Increase = ({ increase, currentNum }) => (
+  <button onClick={increase}> {currentNum} </button>
+);
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello world!
+      <Increase currentNum={counter} increase={() => setCounter(counter + 1)} />
     </div>
   );
 }
