e75118cff0926ddc6d2f714e90a1a732c1c98b61
Ivy <ivyla3@gmail.com>
2021-03-22 16:12:04 -0700

    converted id to the correct data type, RESTful structure

--- NodeServer/pt3/index.js
+++ NodeServer/pt3/index.js
@@ -19,15 +19,35 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         date: "2019-05-30T19:20:14.298Z",
         important: true
+    },
+    {
+        id: 4,
+        content: "GET and POST are the most best methods of Lil Yachtffdsdsdfy",
+        date: "2019-66-30T19:20:14.298Z",
+        important: true
     }
 ]
 
+// Route 1
 app.get("/", (request, response) => {
     response.send("<h1> Hello world! </h1>")
 })
 
-app.get("/api/notes", (request, response) => {
-    response.json(notes)
+// // Route 2
+// app.get("/api/notes/:id", (request, response) => {
+//     const id = request.params.id
+//     const note = notes.find((note) => note.id === id)
+//     console.log(note)
+//     response.json(note)
+// })
+
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find((note) => {
+        return note.id === id
+    })
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
