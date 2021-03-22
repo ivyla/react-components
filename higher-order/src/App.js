bae751547b97277da11607f588abc7b971ccd593
Ivy <ivyla3@gmail.com>
2021-03-22 14:55:56 -0700

    testing express server

--- higher-order/src/App.js
+++ higher-order/src/App.js
@@ -1,25 +1,35 @@
-import "./App.css";
-import React, { useState } from "react";
-import "./Subcomponents/ChildComponents";
-import ChildComponents from "./Subcomponents/ChildComponents";
-import ComplexState from "./Subcomponents/ComplexState";
+import "./App.css"
+import React, { useState } from "react"
+import "./Subcomponents/ChildComponents"
+import ChildComponents from "./Subcomponents/ChildComponents"
+import ComplexState from "./Subcomponents/ComplexState"
+import axios from "axios"
+
 // Use hooks to init the state for the click right and click left buttons
 // Use the spread syntax to initialize and assign the props {//variables here//}
 
 function App() {
-  return (
-    <div>
-      simple example with spread operator
-      {/* <Increase currentNum={firstCounter} increase={() => firstCounter(firstCounter + 1)} /> */}
-      {/* ChildComponents.js */}
-      <div>
-        <ChildComponents />
-      </div>
-      <div>
-        <ComplexState />
-      </div>
-    </div>
-  );
+    const promise = axios
+        .get("http://localhost:3001/notes")
+        .then((response) => console.log(response.data))
+    console.log(promise)
+
+    // const promise2 = axios.get("http://localhost:3001/notes/foobar").then((response) => console.log(response.data))
+    // console.log(promise2)
+
+    return (
+        <div>
+            simple example with spread operator
+            {/* <Increase currentNum={firstCounter} increase={() => firstCounter(firstCounter + 1)} /> */}
+            {/* ChildComponents.js */}
+            <div>
+                <ChildComponents />
+            </div>
+            <div>
+                <ComplexState />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
